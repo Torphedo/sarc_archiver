b3acf00e169f820f25e89e23952e3a1c70845cd4
Torphedo <torphedo.launcher@gmail.com>
2023-06-05 00:46:05 -0400

    Remove most old test code

--- src/main.c
+++ src/main.c
@@ -18,33 +18,11 @@ int main(int argc, char** argv) {
 
   const PHYSFS_ArchiveInfo** i = NULL;
   for (i = PHYSFS_supportedArchiveTypes(); *i != NULL; i++) {
-    printf("Supported archive: [%s], which is [%s].\n", (*i)->extension, (*i)->description);
+    printf("main(): Supported archive: [%s], which is [%s].\n", (*i)->extension, (*i)->description);
   }
 
-  char** rc = PHYSFS_enumerateFiles("/");
-  for (char** i = rc; *i != NULL; i++) {
-    printf(" * We've got [%s].\n", *i);
-  }
-
-  printf("Mounting %s...\n\n", packname);
+  printf("main(): Mounting %s...\n\n", packname);
   PHYSFS_mount(packname, NULL, true);
-
-  rc = PHYSFS_enumerateFiles("/");
-  for (char** i = rc; *i != NULL; i++) {
-    printf(" * We've got [%s].\n", *i);
-  }
-  PHYSFS_freeList(rc);
-
-  PHYSFS_file* file = PHYSFS_openRead("/Component/ArmorParam/Armor_012_Upper.game__component__ArmorParam.bgyml");
-  uint32_t size = PHYSFS_fileLength(file);
-
-  uint8_t* buffer = calloc(1, size);
-  PHYSFS_readBytes(file, buffer, size);
-  PHYSFS_close(file);
-  FILE* output = fopen("Armor_012_Upper.game__component__ArmorParam.bgyml", "wb");
-  fwrite(buffer, size, 1, output);
-  fclose(output);
-  free(buffer);
 
   PHYSFS_file* test_write = PHYSFS_openWrite("/Component/ArmorParam/Armor_012_Upper.game__component__ArmorParam.bgyml");
   PHYSFS_close(test_write);
