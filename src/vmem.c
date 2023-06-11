5e57cc79b469b824a6836cb09a54d59a4a4a7937
S41L0R <60116462+S41L0R@users.noreply.github.com>
2023-06-11 16:56:30 -0400

    compiles for switch (not yet functional)

--- src/vmem.c
+++ src/vmem.c
@@ -16,6 +16,9 @@
 #ifdef _WIN32
 #define PLATFORM_WINDOWS
 #endif
+#ifdef __SWITCH__
+#define PLATFORM_SWITCH
+#endif
 
 // Unix / POSIX implementation
 #if defined(PLATFORM_UNIX) || defined(PLATFORM_MACOS)
@@ -49,4 +52,104 @@ int virtual_free(void* addr, uint64_t size) {
 // Reserving / committing doesn't really exist as a kernel concept on HorizonOS
 // (Switch). We'll probably have to fake this behaviour by doing re-allocation
 // and copying behind the scenes to get it to work.
+#ifdef PLATFORM_SWITCH
+#include <switch/kernel/virtmem.h>
 
+typedef struct ReservationMapping ReservationMapping;
+
+struct ReservationMapping {
+	ReservationMapping* prev;
+	ReservationMapping* next;
+	void* addr;
+	uint64_t size;
+	VirtmemReservation* reservation;
+};
+
+static ReservationMapping* g_ReservationMappings;
+
+void* virtual_reserve(uint64_t size) {
+	virtmemLock();
+	void* addr = virtmemFindAslr(size, 0);
+	VirtmemReservation* reservation = virtmemAddReservation(addr, size);
+	virtmemUnlock();
+
+	ReservationMapping* reservationMapping = malloc(sizeof(ReservationMapping));
+	reservationMapping->prev = NULL;
+	reservationMapping->next = g_ReservationMappings;
+	reservationMapping->addr = addr;
+	reservationMapping->size = size;
+	reservationMapping->reservation = reservation;
+	g_ReservationMappings->prev = reservationMapping;
+	g_ReservationMappings = reservationMapping;
+
+	return addr;
+}
+int virtual_commit(void* addr, uint64_t size) {
+	virtmemLock();
+	VirtmemReservation* reservation = virtmemAddReservation(addr, size);
+	virtmemUnlock();
+
+	ReservationMapping* reservationMapping = malloc(sizeof(ReservationMapping));
+	reservationMapping->prev = NULL;
+	reservationMapping->next = g_ReservationMappings;
+	reservationMapping->addr = addr;
+	reservationMapping->size = size;
+	reservationMapping->reservation = reservation;
+	g_ReservationMappings->prev = reservationMapping;
+	g_ReservationMappings = reservationMapping;
+
+	if (reservation)
+		return 0;
+	else
+		return -1;
+}
+int virtual_free(void* addr, uint64_t size) {
+	VirtmemReservation* reservation;
+
+	for (ReservationMapping* reservationMapping = g_ReservationMappings; reservationMapping; reservationMapping = reservationMapping->next) {
+		if (reservationMapping->addr < (char*)addr + size && (char*)reservationMapping->addr + reservationMapping->size > addr) {
+			virtmemLock();
+
+			virtmemRemoveReservation(reservationMapping->reservation);
+
+			// It's possible that we are freeing just a subsection of what was reserved.
+			// Because of this we may need to keep a segment at the end or beginning reserved, or even both.
+			if (reservationMapping->addr != addr) {
+				VirtmemReservation* leftoverReservation = virtmemAddReservation(reservationMapping->addr, (uint64_t)addr - (uint64_t)reservationMapping->addr);
+				ReservationMapping* leftoverReservationMapping = malloc(sizeof(ReservationMapping));
+				leftoverReservationMapping->prev = NULL;
+				leftoverReservationMapping->next = g_ReservationMappings;
+				leftoverReservationMapping->addr = addr;
+				leftoverReservationMapping->size = size;
+				leftoverReservationMapping->reservation = leftoverReservation;
+				g_ReservationMappings->prev = reservationMapping;
+				g_ReservationMappings = leftoverReservationMapping;
+			}
+			if ((char*)reservationMapping->addr + reservationMapping->size != (char*)addr + size) {
+				VirtmemReservation* leftoverReservation = virtmemAddReservation((char*)addr + size, ((char*)reservationMapping->addr + reservationMapping->size) - ((char*)addr + size));
+				ReservationMapping* leftoverReservationMapping = malloc(sizeof(ReservationMapping));
+				leftoverReservationMapping->prev = NULL;
+				leftoverReservationMapping->next = g_ReservationMappings;
+				leftoverReservationMapping->addr = addr;
+				leftoverReservationMapping->size = size;
+				leftoverReservationMapping->reservation = leftoverReservation;
+				g_ReservationMappings->prev = reservationMapping;
+				g_ReservationMappings = leftoverReservationMapping;
+			}
+
+			// Remove this reservation from the list.
+			if (reservationMapping->next)
+				reservationMapping->next->prev = reservationMapping->prev;
+			if (reservationMapping->prev)
+				reservationMapping->prev->next = reservationMapping->next;
+
+			free(reservationMapping);
+
+			virtmemUnlock();
+			return 0;
+		}
+	}
+
+	return 0; // What was freed was never reserved, so I guess it's a success.
+}
+#endif
